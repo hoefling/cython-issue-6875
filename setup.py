012c845feb72a29aaa280aa326bdc6fe331b6864
Oleg Hoefling <oleg.hoefling@ionos.com>
2025-04-12 14:05:30 +0200

    fix: build the extension for the correct module
    Signed-off-by: Oleg Hoefling <oleg.hoefling@ionos.com>

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import Distribution, Extension, setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules=cythonize([Extension("*", ["spam.py"])], language_level=3, compiler_directives={"linetrace": True}),
+    ext_modules=cythonize([Extension("*", ["eggs.py"])], language_level=3, compiler_directives={"linetrace": True}),
     options={"build_ext": {"define": "CYTHON_TRACE"}},
 )
